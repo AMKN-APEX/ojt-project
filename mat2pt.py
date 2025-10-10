702245c361c26fd8298cded454a52a08c44da874
AMKN APEX <apexbassamkn52@gmail.com>
2025-10-10 22:53:35 +0900

    一つのファイルをmat2ptできるように

--- mat2pt.py
+++ mat2pt.py
@@ -1,8 +1,18 @@
 import os
 import numpy as np
 import h5py
+import torch
 
-path = "/mnt/c/Users/onion/Documents/data/m_train.mat"
 
-f = h5py.File(path, "r")
-print(list(f.keys()))
+mat_path = "/mnt/c/Users/onion/Documents/data/m_train.mat" # MATLABファイルのパス
+pt_path = "/mnt/c/Users/onion/Documents/processed_data/target/m_train.pt" # 保存先の.ptファイルのパス
+
+# pathのディレクトリ部分が存在しない場合は作成、存在してもエラーにしない
+os.makedirs(os.path.dirname(pt_path), exist_ok=True)
+
+# MATLABファイルを読み込み、指定された変数をPyTorchのテンソルに変換して保存
+with h5py.File(mat_path, "r") as f:
+    keys = list(f.keys())
+    data = f[keys]
+    tensor = torch.tensor(data)
+    torch.save(tensor, pt_path)
