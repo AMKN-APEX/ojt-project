e846433d06a1b71ebb479c86d87c5b0e614eefe9
AMKN APEX <apexbassamkn52@gmail.com>
2025-10-13 12:15:42 +0900

    trainのみのDataset, DataLoader完成

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+import os
+import glob
+import re
+import torch
 from torch.utils.data import Dataset, DataLoader
 
 # --- Dataset ---
+DATA_DIR = "/mnt/c/Users/onion/Documents/slice_data_x_y_z_1"
+X_DIR = os.path.join(DATA_DIR, "train")
+Y_PATH = os.path.join(DATA_DIR, "target/m_train.pt")
+
 class PorousDataset(Dataset):
-    def __init__(self, X_data, y_data):
-        self.X = X_data
-        self.y = y_data
+    def __init__(self, X_dir, y_path):
+        files = glob.glob(os.path.join(X_dir, "*.pt"))
+        self.X_files = sorted(files, key=lambda files_path: int(re.findall(r'\d+', os.path.basename(files_path))[0]))
+        self.y = torch.load(y_path)
     
     def __len__(self):
-        return len(self.X)
+        return len(self.X_files)
     
     def __getitem__(self, idx):
-        return self.X[idx], self.y[idx]
-    
-dataset = PorousDataset()
-loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=0)  
+        X = torch.load(self.X_files[idx])
+        y = self.y[idx]
+        return X, y
+
+dataset = PorousDataset(X_DIR, Y_PATH)
+loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=0)
 
 # --- Model ---
 
