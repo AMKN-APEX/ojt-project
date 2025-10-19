bb8500ce72069fec2e4e8a8f7c1c7b7c0523200f
AMKN APEX <apexbassamkn52@gmail.com>
2025-10-19 14:41:59 +0900

    utilsとしてパーサーとオーバーライド部分を分離

--- expr/main.py
+++ expr/main.py
@@ -6,8 +6,6 @@ import torch.nn as nn
 import torch.optim as optim
 import mlflow
 import mlflow.pytorch
-from omegaconf import OmegaConf
-import argparse
 
 # 親ディレクトリをパスに追加
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -15,43 +13,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.dataset import PorousDataset
 from src.model import CNN
 from src.train_val_test import TrainValTest
+from src.utils import cli_override
 
 # --- Load config ---
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-cfg_path = os.path.join(BASE_DIR, "config.yaml")
-cfg = OmegaConf.load(cfg_path)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--data.directory.DATA_DIR", type=str)
-parser.add_argument("--data.data_loader.num_train", type=int)
-parser.add_argument("--data.data_loader.num_val", type=int)
-parser.add_argument("--data.data_loader.num_test", type=int)
-parser.add_argument("--data.data_loader.batch_size", type=int)
-parser.add_argument("--data.data_loader.num_workers", type=int)
-parser.add_argument("--train.learning_rate", type=float)
-parser.add_argument("--train.num_epochs", type=int)
-parser.add_argument("--mlflow.experiment_name", type=str)
-parser.add_argument("--mlflow.run_name", type=str)
-args = parser.parse_args()
-
-# CLI で指定された値だけ上書き
-cli_conf = {k: v for k, v in vars(args).items() if v is not None}
-
-# ドットキーをネスト dict に変換してマージ
-def dict_from_dotkeys(dotdict):
-    result = {}
-    for k, v in dotdict.items():
-        keys = k.split(".")
-        d = result
-        for key in keys[:-1]:
-            if key not in d:
-                d[key] = {}
-            d = d[key]
-        d[keys[-1]] = v
-    return result
-
-cfg = OmegaConf.merge(cfg, OmegaConf.create(dict_from_dotkeys(cli_conf)))
+cfg = cli_override("config.yaml")
 
 # --- mlflow ---
 mlflow.set_tracking_uri(cfg.mlflow.tracking_uri)
