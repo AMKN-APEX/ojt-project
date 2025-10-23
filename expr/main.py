db594f2f625533855b97beb2e404b05976de1e54
AMKN APEX <apexbassamkn52@gmail.com>
2025-10-23 20:31:29 +0900

    pin_memoryを追加

--- expr/main.py
+++ expr/main.py
@@ -40,6 +40,7 @@ def main(cfg: DictConfig) -> None:
     num_test = cfg.data.data_loader.num_test
     batch_size = cfg.data.data_loader.batch_size
     num_workers = cfg.data.data_loader.num_workers
+    pin_memory = cfg.data.data_loader.pin_memory
 
     # data
     m_scaler_name = cfg.data.normalizer.m_scaler_name
@@ -54,6 +55,8 @@ def main(cfg: DictConfig) -> None:
     optimizer_name = cfg.train.optimizer_name
     learning_rate = cfg.train.learning_rate
     num_epochs = cfg.train.num_epochs
+
+    # train.scheduler
     scheduler_name = cfg.train.scheduler.name
     scheduler_params = cfg.train.scheduler.params
 
@@ -70,13 +73,13 @@ def main(cfg: DictConfig) -> None:
     with mlflow.start_run(run_name=run_name):
         # --- DataLoader ---
         train_dataset = PorousDataset(TRAIN_DIR, TRAIN_M_PATH, TRAIN_KAPPA_PATH, num_train, m_scaler_name, kappa_scaler_name)
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
 
         val_dataset = PorousDataset(VAL_DIR, VAL_M_PATH, VAL_KAPPA_PATH, num_val, m_scaler_name, kappa_scaler_name)
-        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
 
         test_dataset = PorousDataset(TEST_DIR, TEST_M_PATH, TEST_KAPPA_PATH, num_test, m_scaler_name, kappa_scaler_name)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
 
         # --- Training and Validation and Test---
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
