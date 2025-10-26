235e8f01d05267aff1a49128ad5b216678917426
AMKN APEX <apexbassamkn52@gmail.com>
2025-10-26 20:09:48 +0900

    変数名変更

--- src/train_val_test.py
+++ src/train_val_test.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 from typing import Optional, Any
 from torch.utils.data import DataLoader
 from torch import nn, optim
-from torch.optim import lr_scheduler
 import mlflow
 
 from .metrics import get_metrics
@@ -28,29 +27,29 @@ class TrainValTest:
 
 
     def run_epoch(self, loader: DataLoader, train: bool = True) -> tuple:
-        loss1_total = 0.0
-        loss2_total = 0.0
+        loss_m_total = 0.0
+        loss_k_total = 0.0
         num_samples = 0
 
         self.model.train(train)
         with torch.set_grad_enabled(train):
-            y1_list = []
-            y2_list = []
-            out1_list = []
-            out2_list = []
+            y_m_list = []
+            y_k_list = []
+            out_m_list = []
+            out_k_list = []
 
             for x, y in loader:
                 x = x.to(self.device, dtype=torch.float32)
-                y1 = y[0].to(self.device, dtype=torch.float32).view(-1, 1) # m
-                y2 = y[1].to(self.device, dtype=torch.float32).view(-1, 1) # kappa
+                y_m = y[0].to(self.device, dtype=torch.float32).view(-1, 1) # m
+                y_k = y[1].to(self.device, dtype=torch.float32).view(-1, 1) # kappa
 
                 outputs = self.model(x)
-                outputs1 = outputs[:, 0].view(-1, 1)
-                outputs2 = outputs[:, 1].view(-1, 1)
+                outputs_m = outputs[:, 0].view(-1, 1)
+                outputs_k = outputs[:, 1].view(-1, 1)
 
-                loss1 = self.criterion(outputs1, y1)
-                loss2 = self.criterion(outputs2, y2)
-                loss = loss1 + loss2
+                loss_m = self.criterion(outputs_m, y_m)
+                loss_k = self.criterion(outputs_k, y_k)
+                loss = loss_m + loss_k
 
                 if train:
                     self.optimizer.zero_grad()
@@ -58,27 +57,27 @@ class TrainValTest:
                     self.optimizer.step()
 
                 batch_size = x.size(0)
-                loss1_total += loss1.item() * batch_size
-                loss2_total += loss2.item() * batch_size
+                loss_m_total += loss_m.item() * batch_size
+                loss_k_total += loss_k.item() * batch_size
 
-                y1_list.append(y1.detach().cpu())
-                y2_list.append(y2.detach().cpu())
-                out1_list.append(outputs1.detach().cpu())
-                out2_list.append(outputs2.detach().cpu())
+                y_m_list.append(y_m.detach().cpu())
+                y_k_list.append(y_k.detach().cpu())
+                out_m_list.append(outputs_m.detach().cpu())
+                out_k_list.append(outputs_k.detach().cpu())
                 num_samples += batch_size
 
-        avg_loss1 = loss1_total / num_samples
-        avg_loss2 = loss2_total / num_samples
+        avg_loss_m = loss_m_total / num_samples
+        avg_loss_k = loss_k_total / num_samples
 
-        y1_all = torch.cat(y1_list, dim=0)
-        out1_all = torch.cat(out1_list, dim=0)
-        y2_all = torch.cat(y2_list, dim=0)
-        out2_all = torch.cat(out2_list, dim=0)
+        y_m_all = torch.cat(y_m_list, dim=0)
+        out_m_all = torch.cat(out_m_list, dim=0)
+        y_k_all = torch.cat(y_k_list, dim=0)
+        out_k_all = torch.cat(out_k_list, dim=0)
 
-        avg_metric1 = get_metrics(self.metrics_name, y1_all, out1_all)
-        avg_metric2 = get_metrics(self.metrics_name, y2_all, out2_all)
+        avg_metric_m = get_metrics(metrics_name=self.metrics_name, y_true=y_m_all, y_pred=out_m_all)
+        avg_metric_k = get_metrics(metrics_name=self.metrics_name, y_true=y_k_all, y_pred=out_k_all)
 
-        return avg_loss1, avg_loss2, avg_metric1, avg_metric2
+        return avg_loss_m, avg_loss_k, avg_metric_m, avg_metric_k
 
 
     def train_val(self):
